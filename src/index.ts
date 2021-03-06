9d3438f234976771e57cbc6137f22f9bb5a95323
TheEmrio <emrio@emrio.fr>
2021-03-06 21:52:44 +0100

    added mongoose

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import config from './config'
 async function main (): Promise<void> {
   process.title = 'JeromeBot'
 
+  if (!process.env.DB_URL) throw new Error('No DB_URL env var found!')
+
+  await mongoose.connect(process.env.DB_URL, config.databaseConfig)
+}
+
 main().catch(err => {
   console.error(err)
   process.exit(1)
